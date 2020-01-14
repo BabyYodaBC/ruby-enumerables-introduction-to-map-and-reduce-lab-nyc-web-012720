467e48d395504e764af1b1e118d2012dd5131d42
ricky wahba <rjwahba@gmail.com>
2020-01-14 01:09:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,7 +46,7 @@ def reduce_to_total(source_array, starting_point)
   total = 0
   counter = 0
   while counter < source_array.length
-  total += source_array[starting_point]
+  total += source_array[counter]
   counter += 1
   end
   total
