9e045b70ad0b0f8eb9e47eaae6a34a5714f3e043
ricky wahba <rjwahba@gmail.com>
2020-01-14 01:00:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,12 +20,13 @@ end
 new_array
 end
 
-def map_to_no_change(source_array)
+def map_to_double(source_array)
   new_array =[]
   counter = 0
   while counter < source_array.length
-  new_array << source_array[counter]
-  counter += 1
-end
-new_array
+  index = (source_array[counter] * 2)
+    new_array.push(index)
+    counter += 1
+  end
+  new_array
 end
