3acf1fc4bad612b8173de1b402687814506f4cd3
ricky wahba <rjwahba@gmail.com>
2020-01-14 01:01:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,11 +31,11 @@ def map_to_double(source_array)
   new_array
 end
 
-def map_to_double(source_array)
+def map_to_square(source_array)
   new_array =[]
   counter = 0
   while counter < source_array.length
-  index = (source_array[counter] * 2)
+  index = (source_array[counter] ^ 2)
     new_array.push(index)
     counter += 1
   end
