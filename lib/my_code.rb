c3d600f9a985c581b4dbf68aa30846e8924ed7f5
ricky wahba <rjwahba@gmail.com>
2020-01-14 00:36:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,13 @@ def map_to_negativize(source_array)
   end
   new_array
 end
+
+def map_to_no_change(source_array)
+  new_array =[]
+  counter = 0
+  while counter < source_array.length
+  new_array << source_array[counter]
+  counter += 1
+end
+new_array
+end
